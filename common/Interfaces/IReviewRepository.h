0fce57c712881a7a14d5108f7bfe719fca8a66ee
Amirali <amirali2000202@email.com>
2026-07-17 13:03:18 +0330

    بازنویسی ReviewRepository و NotificationRepository با اتصال مشترک

---
+++ common/Interfaces/IReviewRepository.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <QString>
+#include <QVector>
+
+namespace bookclub::common {
+
+class Review;
+
+class IReviewRepository {
+public:
+    virtual ~IReviewRepository() = default;
+
+    virtual Review* findById(const QString& id) const = 0;
+    virtual QVector<Review*> findByBook(const QString& bookId) const = 0;
+    virtual QVector<Review*> findByUser(const QString& userId) const = 0;
+    virtual bool save(Review* review) = 0;
+    virtual bool update(Review* review) = 0;
+    virtual bool remove(const QString& id) = 0;
+    virtual double averageRating(const QString& bookId) const = 0;
+    virtual int ratingCount(const QString& bookId) const = 0;
+};
+
+IReviewRepository* createReviewRepository();
+
+} // namespace bookclub::common
